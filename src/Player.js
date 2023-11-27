6e69b1740739a5b3d965b328ebd6c637972f4e09
Tripti Kumari <139527960+tripti7033@users.noreply.github.com>
2023-11-27 06:24:55 +0530

    second commit

---
+++ src/Player.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./player.css";
+
+const Players = () => {
+ return (
+    <div className="players">
+        <div className="player">
+            Player 1: X;
+            </div>
+        <div className="player">
+            Player 2: O;
+        </div>
+    </div>
+  )
+}
+
+export default Players;
